d2df1f2047728d34731d5b73a1492bb28403c040
Knappek <andy.knapp.ak@gmail.com>
2025-02-21 16:16:58 +0100

    finish first draft of setup - requires testing with TAS

--- tf/avi.tf
+++ tf/avi.tf
@@ -22,11 +22,20 @@ resource "avi_cloudconnectoruser" "vcenter_user" {
   }
 }
 
+resource "avi_ipamdnsproviderprofile" "ipam_vip" {
+    name = "ipam_vip"
+    type = "IPAMDNS_TYPE_INTERNAL"
+    internal_profile {
+      usable_networks {
+        nw_ref = avi_network.avi_vip_segment.id
+      }
+    }
+}
 # Create an NSX-T Cloud in Avi
 resource "avi_cloud" "nsxt_cloud" {
-  name                 = "nsx-cloud"
+  name                 = var.avi_cloud_name
   vtype                = "CLOUD_NSXT"
-  maintenance_mode = "true"
+  maintenance_mode = "false"
   obj_name_prefix = "nsx-avi-automated"
   nsxt_configuration {
     nsxt_url     = var.nsxt_host
@@ -36,7 +45,7 @@ resource "avi_cloud" "nsxt_cloud" {
       tz_type        = "OVERLAY"
       transport_zone = data.nsxt_policy_transport_zone.tz.path
       overlay_segment {
-        tier1_lr_id = nsxt_policy_tier1_gateway.t1_router.path
+        tier1_lr_id = nsxt_policy_tier1_gateway.t1_router_avi_mgmt.path
         segment_id  = nsxt_policy_segment.avi_mgmt_segment.path
       }
     }
@@ -49,7 +58,7 @@ resource "avi_cloud" "nsxt_cloud" {
         segment_config_mode = "TIER1_SEGMENT_MANUAL"
         manual {
           tier1_lrs {
-            tier1_lr_id = nsxt_policy_tier1_gateway.t1_router.path
+            tier1_lr_id = nsxt_policy_tier1_gateway.t1_router_avi_vip.path
             segment_id  = nsxt_policy_segment.avi_vip_segment.path
           }
         }
@@ -68,13 +77,20 @@ resource "avi_vcenterserver" "vcenter" {
   cloud_ref               = avi_cloud.nsxt_cloud.id
 }
 
+# This allows enough time tfor NSX Cloud to auto-populate networks and VRF context
+resource "time_sleep" "wait_20_seconds" {
+  depends_on = [avi_cloud.nsxt_cloud]
+  create_duration = "20s"
+}
+
 resource "avi_vrfcontext" "avi_mgmt_vrf" {
-  name        = var.avi_mgmt_vrf_name
+  depends_on = [time_sleep.wait_20_seconds]
+  name        = nsxt_policy_tier1_gateway.t1_router_avi_mgmt.display_name
   cloud_ref   = avi_cloud.nsxt_cloud.id
   static_routes {
     prefix {
       ip_addr {
-        addr = "0.0.0.0/0"
+        addr = "0.0.0.0"
         type = "V4"
       }
       mask = 0
@@ -85,15 +101,20 @@ resource "avi_vrfcontext" "avi_mgmt_vrf" {
     }
     route_id = "1"
   }
+  attrs {
+    key = "tier1path"
+    value = nsxt_policy_tier1_gateway.t1_router_avi_mgmt.path
+  }
 }
 
 resource "avi_vrfcontext" "avi_vip_vrf" {
-  name        = var.avi_vip_vrf_name
+  depends_on = [time_sleep.wait_20_seconds]
+  name        = nsxt_policy_tier1_gateway.t1_router_avi_vip.display_name
   cloud_ref   = avi_cloud.nsxt_cloud.id
   static_routes {
     prefix {
       ip_addr {
-        addr = "0.0.0.0/0"
+        addr = "0.0.0.0"
         type = "V4"
       }
       mask = 0
@@ -104,9 +125,14 @@ resource "avi_vrfcontext" "avi_vip_vrf" {
     }
     route_id = "1"
   }
+  attrs {
+    key = "tier1path"
+    value = nsxt_policy_tier1_gateway.t1_router_avi_vip.path
+  }
 }
 
 resource "avi_network" "avi_mgmt_segment" {
+  depends_on = [time_sleep.wait_20_seconds]
   name        = var.avi_mgmt_segment_name
   vrf_context_ref = avi_vrfcontext.avi_mgmt_vrf.id
   cloud_ref   = avi_cloud.nsxt_cloud.id
@@ -133,9 +159,15 @@ resource "avi_network" "avi_mgmt_segment" {
       type = "STATIC_IPS_FOR_VIP_AND_SE"
     }
   }
+  attrs {
+    key = "segmentid"
+    value = nsxt_policy_segment.avi_mgmt_segment.path
+  }
+  # The below attribute prevents Avi NSX-T Cloud from recreating a new network
 }
 
 resource "avi_network" "avi_vip_segment" {
+  depends_on = [time_sleep.wait_20_seconds]
   name        = var.avi_vip_segment_name
   vrf_context_ref = avi_vrfcontext.avi_vip_vrf.id
   cloud_ref   = avi_cloud.nsxt_cloud.id
@@ -162,60 +194,95 @@ resource "avi_network" "avi_vip_segment" {
       type = "STATIC_IPS_FOR_VIP_AND_SE"
     }
   }
-}
-
-resource "avi_healthmonitor" "web_monitor" {
-  name                = var.avi_health_monitor_name
-  type                = "HEALTH_MONITOR_HTTP"
-  monitor_port        = 8080
-
-  http_monitor {
-    http_request = "GET /health HTTP/1.0"
-    http_response_code = ["HTTP_2XX"]
+  # The below attribute prevents Avi NSX-T Cloud from recreating a new network
+  attrs {
+    key = "segmentid"
+    value = nsxt_policy_segment.avi_vip_segment.path
   }
+  # attrs {
+  #   key = "autocreated"
+  #   value = var.avi_cloud_name
+  # }
+  # attrs {
+  #   key = "cloudnetworkmode"
+  #   value = "static"
+  # }
 }
 
-resource "avi_sslkeyandcertificate" "wildcard_cert" {
-  name         = "tas-wildcard-cert"
-  key = file("${path.module}/wildcard_cert.key")
-  certificate {
-    certificate = file("${path.module}/wildcard_cert.crt")
-  }
-  type= "SSL_CERTIFICATE_TYPE_VIRTUALSERVICE"
-}
-resource "avi_sslkeyandcertificate" "opsman_root_ca" {
-  name         = "opsman_root_ca"
-  certificate {
-    certificate = var.opsman_ca_cert
-  }
-  type= "SSL_CERTIFICATE_TYPE_CA"
-}
+# resource "avi_healthmonitor" "web_monitor" {
+#   name                = var.avi_health_monitor_name
+#   type                = "HEALTH_MONITOR_HTTP"
+#   monitor_port        = 8080
 
-
-# TODO: continue here
-# resource "avi_vsvip" "tas_web" {
-#   name = "tas-web-vip"
-#   cloud_ref = avi_cloud.nsxt_cloud.id
-#   vrf_context_ref = avi_vrfcontext.avi_vip_vrf
-#   vip {
-#     vip_id                    = "0"
-#     auto_allocate_ip          = true
-#     # avi_allocated_vip         = true
-#     auto_allocate_floating_ip = var.floating_ip
-#     availability_zone         = var.aws_availability_zone
-#     subnet_uuid               = data.aws_subnet.terraform-subnets-0.id
-
-#     subnet {
-#       ip_addr {
-#         addr = var.aws_subnet_ip
-#         type = "V4"
-#       }
-
-#       mask = var.aws_subnet_mask
-#     }
+#   http_monitor {
+#     http_request = "GET /health HTTP/1.0"
+#     http_response_code = ["HTTP_2XX"]
 #   }
-
 # }
+
+# resource "avi_sslkeyandcertificate" "wildcard_cert" {
+#   name         = "tas-wildcard-cert"
+#   key = file("${path.module}/wildcard_cert.key")
+#   certificate {
+#     certificate = file("${path.module}/wildcard_cert.crt")
+#   }
+#   type= "SSL_CERTIFICATE_TYPE_VIRTUALSERVICE"
+# }
+# resource "avi_sslkeyandcertificate" "opsman_root_ca" {
+#   name         = "opsman_root_ca"
+#   certificate {
+#     certificate = var.opsman_ca_cert
+#   }
+#   type= "SSL_CERTIFICATE_TYPE_CA"
+# }
+
+
+resource "avi_vsvip" "tas_web" {
+  name = "tas-web-vip"
+  cloud_ref = avi_cloud.nsxt_cloud.id
+  vrf_context_ref = avi_vrfcontext.avi_vip_vrf.id
+  # tier1_lr = nsxt_policy_tier1_gateway.t1_router_avi_vip.display_name
+  vip {
+    vip_id                    = "0"
+    # auto_allocate_ip          = true
+    # avi_allocated_vip         = true
+    # auto_allocate_floating_ip = true
+    # availability_zone         = var.aws_availability_zone
+    # subnet_uuid               = data.aws_subnet.terraform-subnets-0.id
+
+    # TODO: using a static IP here. BUT we actually want to auto_allocate an IP. For this 
+    # you need to create an IPAM profile though. But the issue is: to create an IPAM profile, you need
+    # to reference a network ref, hence you can only do it AFTER the nsx cloud has been created. 
+    # But when creating a cloud, you also need to tell it to use the IPAM profile => chicken-egg problem.
+    # posted in https://chat.google.com/room/AAAAq55Tn0s/wJahepY9r5s/wJahepY9r5s?cls=10
+    ip_address {
+      type = "V4"
+      addr = var.tas_gorouter_vip
+    }
+    subnet {
+      ip_addr {
+        addr = var.avi_vip_segment_ip_addr
+        type = "V4"
+      }
+
+      mask = var.avi_vip_segment_ip_addr_mask
+    }
+  }
+
+}
+
+resource "avi_virtualservice" "dns" {
+  name = "tas-web01"
+  enabled = true
+  vsvip_ref = avi_vsvip.tas_web.id
+  cloud_ref = avi_cloud.nsxt_cloud.id
+  vrf_context_ref = avi_vrfcontext.avi_vip_vrf.id
+  services {
+    port = 443
+  }
+  nsx_securitygroup = [ nsxt_policy_group.gorouters.display_name ]
+  
+}
 
 variable "avi_controller" {
   description = "Avi Controller IP or Hostname"
@@ -239,8 +306,7 @@ variable "avi_tenant" {
   default     = "admin"
 }
 
-variable "avi_mgmt_vrf_name" {}
-variable "avi_vip_vrf_name" {}
+variable "avi_cloud_name" {}
 variable "avi_mgmt_network_ip_addr" {}
 variable "avi_mgmt_network_ip_addr_mask" {}
 variable "avi_mgmt_segment_gateway" {}
@@ -253,3 +319,4 @@ variable "avi_vip_segment_static_ip_begin" {}
 variable "avi_vip_segment_static_ip_end" {}
 variable "avi_health_monitor_name" {}
 variable "opsman_ca_cert" {}
+variable "tas_gorouter_vip" {}
--- tf/nsx-avi.tf
+++ tf/nsx-avi.tf
@@ -33,10 +33,20 @@ data "nsxt_policy_transport_zone" "tz" {
   display_name = var.transport_zone_name
 }
 
-resource "nsxt_policy_tier1_gateway" "t1_router" {
-  display_name              = var.t1_router_name
+resource "nsxt_policy_tier1_gateway" "t1_router_avi_mgmt" {
+  display_name              = var.t1_avi_mgmt_name
   description               = "Terraform provisioned NSX-T Tier-1 Gateway"
-  nsx_id                    = var.t1_router_name
+  nsx_id                    = var.t1_avi_mgmt_name
+  failover_mode             = "NON_PREEMPTIVE"
+  route_advertisement_types = ["TIER1_CONNECTED", "TIER1_STATIC_ROUTES", "TIER1_NAT"]
+  edge_cluster_path         = data.nsxt_policy_edge_cluster.edge_cluster.path
+  tier0_path                = data.nsxt_policy_tier0_gateway.t0_router.path
+}
+
+resource "nsxt_policy_tier1_gateway" "t1_router_avi_vip" {
+  display_name              = var.t1_avi_vip_name
+  description               = "Terraform provisioned NSX-T Tier-1 Gateway"
+  nsx_id                    = var.t1_avi_vip_name
   failover_mode             = "NON_PREEMPTIVE"
   route_advertisement_types = ["TIER1_CONNECTED", "TIER1_STATIC_ROUTES", "TIER1_NAT"]
   edge_cluster_path         = data.nsxt_policy_edge_cluster.edge_cluster.path
@@ -46,7 +56,7 @@ resource "nsxt_policy_tier1_gateway" "t1_router" {
 resource "nsxt_policy_segment" "avi_mgmt_segment" {
   display_name        = var.avi_mgmt_segment_name
   description         = "Terraform provisioned NSX-T Segment for Avi Management"
-  connectivity_path   = nsxt_policy_tier1_gateway.t1_router.path
+  connectivity_path   = nsxt_policy_tier1_gateway.t1_router_avi_mgmt.path
   transport_zone_path = data.nsxt_policy_transport_zone.tz.path
   subnet {
     cidr = "${var.avi_mgmt_segment_gateway}/${var.avi_mgmt_network_ip_addr_mask}"
@@ -56,7 +66,7 @@ resource "nsxt_policy_segment" "avi_mgmt_segment" {
 resource "nsxt_policy_segment" "avi_vip_segment" {
   display_name        = var.avi_vip_segment_name
   description         = "Terraform provisioned NSX-T Segment for Avi VIP"
-  connectivity_path   = nsxt_policy_tier1_gateway.t1_router.path
+  connectivity_path   = nsxt_policy_tier1_gateway.t1_router_avi_vip.path
   transport_zone_path = data.nsxt_policy_transport_zone.tz.path
   subnet {
     cidr = "${var.avi_vip_segment_gateway}/${var.avi_vip_segment_ip_addr_mask}"
@@ -71,17 +81,10 @@ resource "nsxt_policy_group" "gorouters" {
 variable "nsxt_host" {}
 variable "nsxt_username" {}
 variable "nsxt_password" {}
-variable "t1_router_name" {}
+variable "t1_avi_mgmt_name" {}
+variable "t1_avi_vip_name" {}
 variable "edge_cluster_name" {}
 variable "t0_router_name" {}
 variable "transport_zone_name" {}
 variable "avi_mgmt_segment_name" {}
 variable "avi_vip_segment_name" {}
-
-output "t1_router_id" {
-  value = nsxt_policy_tier1_gateway.t1_router.id
-}
-
-output "transport_zone_type" {
-  value = data.nsxt_policy_transport_zone.tz.transport_type
-}
