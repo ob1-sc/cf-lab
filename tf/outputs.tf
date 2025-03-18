34f6bbf6e1d9097abd2f9ecdd56cbe7602c3820c
Knappek <andy.knapp.ak@gmail.com>
2025-03-18 16:25:46 +0100

    update vip network

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -5,6 +5,7 @@ locals {
     management_network_segment_id = nsxt_policy_segment.avi_mgmt_segment.path
     vip_network_transport_zone = data.nsxt_policy_transport_zone.tz.path
     vip_network_tier1_lr_id = nsxt_policy_tier1_gateway.t1_router_avi_vip.path
+    vip_network_tier1_lr_display_name = nsxt_policy_tier1_gateway.t1_router_avi_vip.display_name
     vip_network_segment_id = nsxt_policy_segment.avi_vip_segment.path
   }
 }
